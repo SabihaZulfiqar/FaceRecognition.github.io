e4b138b918771d38afe06a6467d4d74be07e6e6b
Sabiha Zulfiqar <36043559+SabihaZulfiqar@users.noreply.github.com>
2019-04-29 12:42:06 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ class App extends Component {
 
    onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('https://gentle-tundra-27533.herokuapp.com/imageurl', {
+      fetch('https://secret-fjord-10029.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -82,7 +82,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://gentle-tundra-27533.herokuapp.com:3000/image', {
+          fetch('https://secret-fjord-10029.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
