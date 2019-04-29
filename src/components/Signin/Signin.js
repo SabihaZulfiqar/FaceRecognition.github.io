58f831ac62e5f8bd1f175b32acc2afb50cf015df
Sabiha Zulfiqar <36043559+SabihaZulfiqar@users.noreply.github.com>
2019-04-29 12:39:56 +0400

    Update Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
     }
 
     onSubmitSignIn = () => {
-      fetch('https://gentle-tundra-27533.herokuapp.com/signin' , {
+      fetch('https://secret-fjord-10029.herokuapp.com/signin' , {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
