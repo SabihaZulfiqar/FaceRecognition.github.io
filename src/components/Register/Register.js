3510808feabe244aa5d75504d72e713502a5446f
Sabiha Zulfiqar <36043559+SabihaZulfiqar@users.noreply.github.com>
2019-04-30 16:37:16 +0400

    Update Register.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
     }
 
   onSubmitSignIn = () => {
-    fetch('http://secure-garden-69199.herokuapp.com/register', {
+    fetch('https://secure-garden-69199.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
